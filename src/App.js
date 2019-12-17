9c581dc202bb3d58eb22c4eec3ea096c00fd9dba
Vitalii <narodnyjpikap@gmail.com>
2019-12-17 11:58:27 +0200

    Started dialog menu

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
+import Header from "./components/Header/Header";
+import NavBar from "./components/Navbar/Navbar";
+import Profile from "./components/Profile/Profile";
+import Dialogs from "./components/Dialogs/Dialogs";
 
-const App = () =>{
-  return (
-      <div>
+const App = () => {
+    return (
+        <div className='app-wrapper'>
+            <Header/>
+            <NavBar/>
+            <div className='app-wrapper-content'>
+                <Dialogs/>
+            </div>
 
-      </div>
-  );
+            {/*<Profile/>*/}
+
+
+        </div>
+    );
 };
 
 
