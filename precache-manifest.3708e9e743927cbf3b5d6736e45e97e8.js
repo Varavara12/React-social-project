ffed04141786ca41738b0f3042f31d6e5eab2c78
Vitalii <narodnyjpikap@gmail.com>
2020-02-05 18:35:18 +0200

    Updates

---
+++ precache-manifest.3708e9e743927cbf3b5d6736e45e97e8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85f7a35a075804da29a17c6c4439d474",
+    "url": "/React-social-project/index.html"
+  },
+  {
+    "revision": "e5dce1c3b94856deb65a",
+    "url": "/React-social-project/static/css/3.c6a8dd5f.chunk.css"
+  },
+  {
+    "revision": "f3b0f30637074b0ec6a0",
+    "url": "/React-social-project/static/css/4.b4924b5f.chunk.css"
+  },
+  {
+    "revision": "791dc0b6f85f880bd308",
+    "url": "/React-social-project/static/css/main.817bfdf3.chunk.css"
+  },
+  {
+    "revision": "2e8c0f376097239082a5",
+    "url": "/React-social-project/static/js/2.a6f9ff73.chunk.js"
+  },
+  {
+    "revision": "813a21a075d7882287c945d4de71380f",
+    "url": "/React-social-project/static/js/2.a6f9ff73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e5dce1c3b94856deb65a",
+    "url": "/React-social-project/static/js/3.77f118c8.chunk.js"
+  },
+  {
+    "revision": "f3b0f30637074b0ec6a0",
+    "url": "/React-social-project/static/js/4.b9f567b9.chunk.js"
+  },
+  {
+    "revision": "791dc0b6f85f880bd308",
+    "url": "/React-social-project/static/js/main.4fb54b89.chunk.js"
+  },
+  {
+    "revision": "cf99f377c473d2c7bf2e",
+    "url": "/React-social-project/static/js/runtime-main.700acac8.js"
+  },
+  {
+    "revision": "d4b1d2f8f8ddbca6c8689be27bb2b2fb",
+    "url": "/React-social-project/static/media/login-bg.d4b1d2f8.jpg"
+  },
+  {
+    "revision": "b3b9fe57bef5cd6543681c3c67188624",
+    "url": "/React-social-project/static/media/logo.b3b9fe57.png"
+  },
+  {
+    "revision": "3b54532244fe4bc6a0e2b5a333406291",
+    "url": "/React-social-project/static/media/rolling.3b545322.svg"
+  },
+  {
+    "revision": "57b075ed8d911cb5f5e5951923fba463",
+    "url": "/React-social-project/static/media/users.57b075ed.png"
+  }
+]);
